eb1a9ecf1f8f4a1542c2db3960c75297fc07045b
Tan Yi Jun <tanyijun6@gmail.com>
2024-04-09 22:25:29 +0800

    Test driver script

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 import { Tree } from "./BST.js";
 
+// Driver script
+(function () {
+  const array = [];
+  for (let i = 0; i < Math.floor(Math.random() * 90 + 10); i++) {
+    array.push(Math.round(Math.random() * 100));
+  }
+
+  const tree = Tree(array);
+  console.log('Is the tree balanced?');
+  console.log(tree.isBalanced());
+  console.log('Level order print:');
+  console.log(tree.levelOrder());
+  console.log('Pre order print:');
+  console.log(tree.preOrder());
+  console.log('Post order print:');
+  console.log(tree.postOrder());
+  console.log('In order print:');
+  console.log(tree.inOrder());
+  console.log('Adding some elements to unbalance the tree...')
+  for (let i = 0; i < 10; i++) {
+    tree.insert(Math.round(Math.random() * 100 + 100));
+  }
+  console.log('Is the tree balanced now?');
+  console.log(tree.isBalanced());
+  console.log('Now balance the tree.');
+  console.log('Balancing tree...')
+  tree.rebalance();
+  console.log('Is the tree balanced now?');
+  console.log(tree.isBalanced());
+  console.log('Level order print:');
+  console.log(tree.levelOrder());
+  console.log('Pre order print:');
+  console.log(tree.preOrder());
+  console.log('Post order print:');
+  console.log(tree.postOrder());
+  console.log('In order print:');
+  console.log(tree.inOrder());
+})();
+
+/** Test script
+
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = Tree(array);
 tree.insert(15);
@@ -27,3 +68,5 @@ console.log('balancing...');
 tree.rebalance();
 console.log(tree.isBalanced());
 tree.toString();
+
+ */
